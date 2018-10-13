66df54060b7c6eaf6b15dc13dae34b7bf66cc840
matsueushi <sinxcosxtanx@gmail.com>
2018-10-13 14:45:04 -0400

    persistent stack

--- src/stack.rs
+++ src/stack.rs
@@ -3,23 +3,27 @@
 trait Stack<T>: Sized {
     fn empty() -> Self;
     fn is_empty(&self) -> bool;
-    fn cons(&mut self, _: T) -> &mut Self;
+    fn cons(&self, elt: T) -> Self;
     fn head(&self) -> Option<&T>;
-    fn tail(&mut self) -> Option<&mut Self>;
+    fn tail(&self) -> Option<Self>;
 }
 
-enum StackNode<T> {
+#[derive(Clone)]
+enum StackNode<T: Clone> {
     Cons(T, Box<StackNode<T>>),
     Nil,
 }
 
-struct CustomStack<T> {
+struct CustomStack<T: Clone> {
     node: StackNode<T>,
 }
 
 use self::StackNode::*;
 
-impl<T> Stack<T> for CustomStack<T> {
+impl<T> Stack<T> for CustomStack<T>
+where
+    T: Clone,
+{
     fn empty() -> CustomStack<T> {
         CustomStack { node: Nil }
     }
@@ -31,10 +35,10 @@ impl<T> Stack<T> for CustomStack<T> {
         }
     }
 
-    fn cons(&mut self, elt: T) -> &mut CustomStack<T> {
-        self.node = Cons(elt, Box::new(::std::mem::replace(&mut self.node, Nil)));
-        self
+    fn cons(&self, elt: T) -> CustomStack<T> {
+        CustomStack { node: Cons(elt, Box::new(self.node.clone())) }
     }
+
     fn head(&self) -> Option<&T> {
         match self.node {
             Nil => None,
@@ -42,24 +46,24 @@ impl<T> Stack<T> for CustomStack<T> {
         }
     }
 
-    fn tail(&mut self) -> Option<&mut CustomStack<T>> {
-        match &mut self.node {
+    fn tail(&self) -> Option<CustomStack<T>> {
+        match &self.node {
             Nil => None,
-            Cons(_, x) => {
-                self.node = ::std::mem::replace(x, Nil);
-                Some(self)
-            }
+            Cons(_, x) => Some(CustomStack { node: *x.clone() }),
         }
     }
 }
 
 use std::collections::LinkedList;
 
-struct LinkedListStack<T> {
+struct LinkedListStack<T: Clone> {
     node: LinkedList<T>,
 }
 
-impl<T> Stack<T> for LinkedListStack<T> {
+impl<T> Stack<T> for LinkedListStack<T>
+where
+    T: Clone,
+{
     fn empty() -> LinkedListStack<T> {
         LinkedListStack { node: LinkedList::new() }
     }
@@ -68,41 +72,47 @@ impl<T> Stack<T> for LinkedListStack<T> {
         self.node.is_empty()
     }
 
-    fn cons(&mut self, elf: T) -> &mut LinkedListStack<T> {
-        self.node.push_front(elf);
-        self
+    fn cons(&self, elf: T) -> LinkedListStack<T> {
+        let mut copy = self.node.clone();
+        copy.push_front(elf);
+        LinkedListStack { node: copy }
     }
 
     fn head(&self) -> Option<&T> {
         self.node.front()
     }
 
-    fn tail(&mut self) -> Option<&mut LinkedListStack<T>> {
-        match self.node.pop_front() {
+    fn tail(&self) -> Option<LinkedListStack<T>> {
+        let mut copy = self.node.clone();
+        match copy.pop_front() {
             None => None,
-            Some(_) => Some(self),
+            Some(_) => Some(LinkedListStack { node: copy }),
         }
     }
 }
 
-#[test]
-fn custom_stack_test() {
-    let stack: CustomStack<i32> = CustomStack::empty();
-    assert_eq!(stack.is_empty(), true);
-    let mut stack2 = CustomStack::empty();
-    let stack2 = stack2.cons(1).cons(2);
-    assert_eq!(stack2.is_empty(), false);
-    assert_eq!(stack2.head(), Some(&2));
-    assert_eq!(stack2.tail().unwrap().head(), Some(&1));
-}
+#[cfg(test)]
+mod tests {
 
-#[test]
-fn linked_list_stack_test() {
-    let stack: LinkedListStack<i32> = LinkedListStack::empty();
-    assert_eq!(stack.is_empty(), true);
-    let mut stack2 = LinkedListStack::empty();
-    let stack2 = stack2.cons(1).cons(2);
-    assert_eq!(stack2.is_empty(), false);
-    assert_eq!(stack2.head(), Some(&2));
-    assert_eq!(stack2.tail().unwrap().head(), Some(&1));
+    use super::*;
+
+    #[test]
+    fn custom_stack_test() {
+        let stack: CustomStack<i32> = CustomStack::empty();
+        assert_eq!(stack.is_empty(), true);
+        let stack2 = CustomStack::empty().cons(1).cons(2);
+        assert_eq!(stack2.is_empty(), false);
+        assert_eq!(stack2.head(), Some(&2));
+        assert_eq!(stack2.tail().unwrap().head(), Some(&1));
+    }
+
+    #[test]
+    fn linked_list_stack_test() {
+        let stack: LinkedListStack<i32> = LinkedListStack::empty();
+        assert_eq!(stack.is_empty(), true);
+        let stack2 = LinkedListStack::empty().cons(1).cons(2);
+        assert_eq!(stack2.is_empty(), false);
+        assert_eq!(stack2.head(), Some(&2));
+        assert_eq!(stack2.tail().unwrap().head(), Some(&1));
+    }
 }
