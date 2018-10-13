cf7231c15fca2ab886665b1657a56cd0296d1b1a
matsueushi <sinxcosxtanx@gmail.com>
2018-10-12 23:11:56 -0400

    generics of the function suffixes

--- src/excercise2_1.rs
+++ src/excercise2_1.rs
@@ -1,12 +1,12 @@
 use std::collections::LinkedList;
 
 /// Returns a list of suffixes sorted in descending order of length
-fn suffixes(lst: &LinkedList<i32>) -> LinkedList<LinkedList<i32>> {
+fn suffixes<T: Clone>(lst: &LinkedList<T>) -> LinkedList<LinkedList<T>> {
     let mut suf_lst = LinkedList::new();
     let mut rest_lst = lst.clone();
     loop {
         suf_lst.push_back(rest_lst.clone());
-        if rest_lst.pop_front() == None {
+        if rest_lst.pop_front().is_none() {
             break;
         }
     }
@@ -22,5 +22,4 @@ fn suffixes_test() {
     lst.push_back(4);
     println!("list: {:?}", lst);
     println!("suffixes(&lst): {:?}", suffixes(&lst));
-
 }
