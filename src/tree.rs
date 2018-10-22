6f40c4eb94f26cf564a6c419130924fd5920cc21
matsueushi <sinxcosxtanx@gmail.com>
2018-10-21 22:51:49 -0400

    singleton for tree

--- src/tree.rs
+++ src/tree.rs
@@ -20,6 +20,16 @@ pub enum Tree<T: Clone> {
 
 use self::Tree::*;
 
+impl<T: Clone> Tree<T> {
+    fn singleton(elt: T) -> Tree<T> {
+        Node {
+            val: elt,
+            ltree: Rc::new(Empty),
+            rtree: Rc::new(Empty),
+        }
+    }
+}
+
 impl<T: Clone + PartialOrd> Set<T> for Tree<T> {
     fn empty() -> Tree<T> {
         Empty
@@ -27,72 +37,60 @@ impl<T: Clone + PartialOrd> Set<T> for Tree<T> {
 
     // Section 2.2 first version
 
-    //    fn insert(&self, elt: T) -> Tree<T> {
-    //        match self {
-    //            Empty => Node {
-    //                val: elt,
-    //                ltree: Rc::new(Empty),
-    //                rtree: Rc::new(Empty),
-    //            },
-    //            Node { val, ltree, rtree } => {
-    //                if elt < *val {
-    //                    Node {
-    //                        val: val.clone(),
-    //                        ltree: Rc::new(ltree.insert(elt)),
-    //                        rtree: Rc::clone(&rtree),
-    //                    }
-    //                } else if elt > *val {
-    //                    Node {
-    //                        val: val.clone(),
-    //                        ltree: Rc::clone(&ltree),
-    //                        rtree: Rc::new(rtree.insert(elt)),
-    //                    }
-    //                } else {
-    //                    self.clone()
-    //                }
-    //            }
-    //        }
-    //    }
+    // fn insert(&self, elt: T) -> Tree<T> {
+    //     match self {
+    //         Empty => Self::singleton(elt),
+    //         Node { val, ltree, rtree } => {
+    //             if elt < *val {
+    //                 Node {
+    //                     val: val.clone(),
+    //                     ltree: Rc::new(ltree.insert(elt)),
+    //                     rtree: Rc::clone(&rtree),
+    //                 }
+    //             } else if elt > *val {
+    //                 Node {
+    //                     val: val.clone(),
+    //                     ltree: Rc::clone(&ltree),
+    //                     rtree: Rc::new(rtree.insert(elt)),
+    //                 }
+    //             } else {
+    //                 self.clone()
+    //             }
+    //         }
+    //     }
+    // }
 
     // Exercise 2.3
 
-    //    fn insert(&self, elt: T) -> Tree<T> {
-    //        fn insert_impl<T: Clone + PartialOrd>(tree: &Tree<T>, elt: T) -> Option<Tree<T>> {
-    //            match tree {
-    //                Empty => Some(Node {
-    //                    val: elt,
-    //                    ltree: Rc::new(Empty),
-    //                    rtree: Rc::new(Empty),
-    //                }),
-    //                Node { val, ltree, rtree } => {
-    //                    if elt < *val {
-    //                        insert_impl(&ltree, elt).map(|x| {
-    //                            Node {
-    //                                val: val.clone(),
-    //                                ltree: Rc::new(x),
-    //                                rtree: Rc::clone(&rtree),
-    //                            }
-    //                        })
-    //                    } else if elt > *val {
-    //                        insert_impl(&rtree, elt).map(|x| {
-    //                            Node {
-    //                                val: val.clone(),
-    //                                ltree: Rc::clone(&ltree),
-    //                                rtree: Rc::new(x),
-    //                            }
-    //                        })
-    //                    } else {
-    //                        None
-    //                    }
-    //                }
-    //            }
-    //        }
-    //
-    //    match insert_impl(&self, elt) {
-    //        Some(x) => x,
-    //        None => self.clone(),
-    //    }
-    //}
+    // fn insert(&self, elt: T) -> Tree<T> {
+    //     fn insert_impl<T: Clone + PartialOrd>(tree: &Tree<T>, elt: T) -> Option<Tree<T>> {
+    //         match tree {
+    //             Empty => Some(Tree::singleton(elt)),
+    //             Node { val, ltree, rtree } => {
+    //                 if elt < *val {
+    //                     insert_impl(&ltree, elt).map(|x| Node {
+    //                         val: val.clone(),
+    //                         ltree: Rc::new(x),
+    //                         rtree: Rc::clone(&rtree),
+    //                     })
+    //                 } else if elt > *val {
+    //                     insert_impl(&rtree, elt).map(|x| Node {
+    //                         val: val.clone(),
+    //                         ltree: Rc::clone(&ltree),
+    //                         rtree: Rc::new(x),
+    //                     })
+    //                 } else {
+    //                     None
+    //                 }
+    //             }
+    //         }
+    //     }
+
+    //     match insert_impl(&self, elt) {
+    //         Some(x) => x,
+    //         None => self.clone(),
+    //     }
+    // }
 
     // Exercise 2.4
 
@@ -107,11 +105,7 @@ impl<T: Clone + PartialOrd> Set<T> for Tree<T> {
                     if elt == *possible {
                         None
                     } else {
-                        Some(Node {
-                            val: elt,
-                            ltree: Rc::new(Empty),
-                            rtree: Rc::new(Empty),
-                        })
+                        Some(Tree::singleton(elt))
                     }
                 }
                 Node { val, ltree, rtree } => {
@@ -133,11 +127,7 @@ impl<T: Clone + PartialOrd> Set<T> for Tree<T> {
         }
 
         match self {
-            Empty => Node {
-                val: elt,
-                ltree: Rc::new(Empty),
-                rtree: Rc::new(Empty),
-            },
+            Empty => Self::singleton(elt),
             Node { val, .. } => match insert_impl(&self, elt, &val) {
                 Some(x) => x,
                 None => self.clone(),
